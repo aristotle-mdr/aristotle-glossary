344c76a82c3a9a98e025a82a059be2f156c383fe
Samuel Spencer <theodore.therone@gmail.com>
2016-04-24 11:59:19 +0000

    fix pip

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
     install_requires = [
-        'aristotle-metadata-registry>=1.2.0'
+        'aristotle-metadata-registry>=1.2.3'
     ]
 
 )
