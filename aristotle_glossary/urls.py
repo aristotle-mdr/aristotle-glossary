cff56a7f1a3b1cdabd3bf6c50023d6c2e25692fc
Samuel Spencer <theodore.therone@gmail.com>
2016-09-11 22:07:57 +0000

    fix urls

--- aristotle_glossary/urls.py
+++ aristotle_glossary/urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.views.generic import TemplateView
 
 from aristotle_glossary import views
 
 # Recommended to put this at "/glossary" when including these URLs
-urlpatterns = patterns('aristotle_glossary.views',
+urlpatterns = [
     url(r'^/?$', views.glossary, name='glossary'),
     url(r'^jsonlist/', views.json_list, name='json_list'),
     url(r'^search_dialog/?$',  views.search_dialog, name='search_dialog'),
     url(r'^about/(?P<template>.+)/?$', views.DynamicTemplateView.as_view(), name="about"),
-)
+]
