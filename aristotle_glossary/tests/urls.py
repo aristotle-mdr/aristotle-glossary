26c041981e80b717b5be7a59cba6ded0329814e7
Samuel Spencer <theodore.therone@gmail.com>
2017-11-02 05:40:09 +0000

    add urls

--- aristotle_glossary/tests/urls.py
+++ aristotle_glossary/tests/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import include, url
 
 urlpatterns = [
     url(r'^', include('aristotle_mdr.urls')),
+    url(r'^', include('aristotle_mdr.contrib.slots.urls', app_name="aristotle_slots", namespace="aristotle_slots")),
+    url(r'^browse/', include('aristotle_mdr.contrib.browse.urls')),
+    url(r'^help/', include('aristotle_mdr.contrib.help.urls', app_name="aristotle_help", namespace="aristotle_help")),
     url(r'^glossary/', include('aristotle_glossary.urls',app_name="aristotle_glossary",namespace="glossary")),
 ]
